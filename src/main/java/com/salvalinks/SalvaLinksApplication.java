154cba7f3d1359d1ebfb798042fba4d482510892
thallysonjsa <thallyson.alves@ccc.ufcg.edu.br>
2019-04-29 19:35:02 -0300

    edições básicas na classe principal

--- src/main/java/com/salvalinks/SalvaLinksApplication.java
+++ src/main/java/com/salvalinks/SalvaLinksApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.salvalinks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SalvalinksApiApplication {
+public class SalvaLinksApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SalvalinksApiApplication.class, args);
+		SpringApplication.run(SalvaLinksApplication.class, args);
 	}
 
 }
